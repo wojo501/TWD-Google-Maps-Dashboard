72582868cdd2faad13cc9ae9bf81e019d8e8b2dd
tymsoncyferki <tymekurban@gmail.com>
2023-01-24 18:24:07 +0100

    aktualne dane na mapie

--- app.R
+++ app.R
@@ -12,8 +12,8 @@ options(shiny.usecairo=T)
 useShinyjs()
 
 map_df <- read.csv("map_data/map_df.csv", encoding = 'UTF-8')
-filterData <- readRDS(file = "ramkiW/data.rds")
-baseFrame <- readRDS(file = "ramkiW/baseFrame.rds")
+filterData <- readRDS(file = "time_data/data.rds")
+baseFrame <- readRDS(file = "time_data/baseFrame.rds")
 trans_df <- read.csv("trans_data/dataC", encoding = 'UTF-8')
 
 
@@ -104,8 +104,8 @@ map_ui <- fluidPage(
            ),
            
            dateRangeInput("date_range", "Select date range:",
-                          start = "2022-12-07", end = "2023-01-05",
-                          min = "2022-12-07", max = "2023-01-05",
+                          start = "2022-12-07", end = "2023-01-24",
+                          min = "2022-12-07", max = "2023-01-24",
                           format = "yyyy-mm-dd", startview = "month",
                           autoclose = TRUE)),
            br(),
